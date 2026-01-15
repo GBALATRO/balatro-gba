5b219de262785108988c5882f903f467548a54e7
emiyl <me@emiyl.com>
2026-01-15 17:56:56 +0000

    create separate headers for palettes, rects and timer defs

---
+++ include/game/palette.h
@@ -0,0 +1,32 @@
+#ifndef PALETTE_H
+#define PALETTE_H
+
+// Background Palette IDs
+#define BOSS_BLIND_PRIMARY_PID               1
+#define MAIN_MENU_PLAY_BUTTON_OUTLINE_PID    2
+#define REROLL_BTN_PID                       3
+#define BLIND_SKIP_BTN_PID                   5
+#define MAIN_MENU_PLAY_BUTTON_MAIN_COLOR_PID 5
+#define NEXT_ROUND_BTN_SELECTED_BORDER_PID   5
+#define BLIND_BG_SHADOW_PID                  5
+#define SHOP_PANEL_SHADOW_PID                6
+#define PLAY_HAND_BTN_PID                    6
+#define BOSS_BLIND_SHADOW_PID                7
+#define PLAY_HAND_BTN_BORDER_PID             7
+#define REROLL_BTN_SELECTED_BORDER_PID       7
+#define SHOP_LIGHTS_1_PID                    8
+#define DISCARD_BTN_BORDER_PID               8
+#define BLIND_SKIP_BTN_SELECTED_BORDER_PID   10
+#define DISCARD_BTN_PID                      13
+#define SHOP_LIGHTS_2_PID                    14
+#define BLIND_SELECT_BTN_PID                 15
+#define NEXT_ROUND_BTN_PID                   16
+#define SHOP_LIGHTS_3_PID                    17
+#define BLIND_SELECT_BTN_SELECTED_BORDER_PID 18
+#define BLIND_BG_SECONDARY_PID               18
+#define BLIND_BG_PRIMARY_PID                 19
+#define REWARD_PANEL_BORDER_PID              19
+#define SHOP_LIGHTS_4_PID                    22
+#define SHOP_BOTTOM_PANEL_BORDER_PID         26
+
+#endif // PALETTE_H
---
+++ include/game/rect.h
@@ -0,0 +1,57 @@
+#ifndef RECT_H
+#define RECT_H
+
+#include "graphic_utils.h"
+
+// Game rects - Screenblock rects (in tiles)
+// Round end/shop menus
+static const Rect ROUND_END_MENU_RECT       = {9,       7,      24,     20 };
+static const Rect POP_MENU_ANIM_RECT        = {9,       7,      24,     31 };
+
+// Blind select
+static const Rect SINGLE_BLIND_SELECT_RECT  = {9,       7,      13,     31 };
+static const Rect BLIND_SKIP_BTN_GRAY_RECT  = {0,       24,     4,      27 };
+static const Rect BLIND_SKIP_BTN_PREANIM_DEST_RECT = {9,29,     19,     31 };
+static const Rect SINGLE_BLIND_SEL_REQ_SCORE_RECT = {80, 120, 104, 128};
+
+// Hand background
+static const Rect HAND_BG_RECT_SELECTING    = {9,       11,     24,     17 };
+
+// Top left panel
+static const Rect TOP_LEFT_ITEM_SRC_RECT    = {0,       20,     8,      25 };
+static const BG_POINT TOP_LEFT_PANEL_POINT  = {0,       0, };
+static const Rect TOP_LEFT_PANEL_ANIM_RECT  = {0,       0,      8,      4  };
+static const Rect TOP_LEFT_PANEL_BOTTOM_ROW_RESET_RECT = {0, 28, 8,     28 };
+static const BG_POINT TOP_LEFT_BLIND_TITLE_POINT = {0,  21, };
+static const Rect BIG_BLIND_TITLE_SRC_RECT  = {0,       26,     8,      26 };
+static const Rect BOSS_BLIND_TITLE_SRC_RECT = {0,       27,     8,      27 };
+
+// Cashout and game over dialogs
+static const Rect CASHOUT_DEST_RECT =         {10,      8,      23,     10 };
+static const BG_POINT CASHOUT_SRC_3X3_RECT_POS =   {5,  29};
+static const BG_POINT GAME_OVER_SRC_RECT_3X3_POS = {25, 29};
+static const Rect GAME_OVER_DIALOG_DEST_RECT= {11,      21,      23,     28};
+static const Rect GAME_OVER_ANIM_RECT       = {11,      8,       23,     28};
+static const BG_POINT NEW_RUN_BTN_DEST_POS  = {15,      26};
+static const Rect NEW_RUN_BTN_SRC_RECT      = {0,       30,      4,      31};
+static const BG_POINT ROUND_END_REWARDS_ELLIPSIS_POS = {10, 13};
+
+// Score flames animation
+static const Rect SCORE_FLAME_RESET         = {26,      20,      28,     20};
+static const Rect SCORE_FLAME_FRAMES_START  = {26,      21,      28,     21};
+static const BG_POINT SCORE_FLAME_CHIPS_POS = {1,       9};
+static const BG_POINT SCORE_FLAME_MULT_POS  = {5,       9};
+
+// TTE rects (in pixels)
+static const Rect HAND_SIZE_RECT            = {128,     128,    152,    160 };
+static const Rect HAND_SIZE_RECT_SELECT     = {128,     128,    152,    136 };
+static const Rect HAND_SIZE_RECT_PLAYING    = {128,     152,    152,    160 };
+static const Rect HAND_TYPE_RECT            = {8,       64,     64,     72  };
+static const Rect TEMP_SCORE_RECT           = {8,       64,     64,     72  };
+static const Rect SCORE_RECT                = {24,      48,     64,     56  };
+
+// Other rects
+static const Rect FULL_SCREENBLOCK_RECT     = {0,       0,      30,     31  };
+static const Rect COUNTDOWN_TIMER_RECT      = {208,     144,    240,    152 };
+
+#endif // RECT_H
