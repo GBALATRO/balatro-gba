1ad0f617d50caf122dedf342e481e98107f02b78
Rickey Fehr <ric@rf3.xyz>
2026-03-29 18:49:31 -0700

    clang format

--- source/game.c
+++ source/game.c
@@ -1,7 +1,5 @@
 #include "game.h"
 
-#include "game/common_ui.h"
-
 #include "affine_background.h"
 #include "affine_background_gfx.h"
 #include "audio_utils.h"
@@ -3745,8 +3743,10 @@ static inline void game_round_end_print_interest_reward(int interest_y_offset)
         );
     }
     // Increment the interest reward text until the interest reward variable is depleted
-    else if (timer > interest_start_time + TM_REWARD_DISPLAY_INTERVAL &&
-             timer % FRAMES(TM_REWARD_INCREMENT_INTERVAL) == 0)
+    else if (
+        timer > interest_start_time + TM_REWARD_DISPLAY_INTERVAL &&
+        timer % FRAMES(TM_REWARD_INCREMENT_INTERVAL) == 0
+    )
     {
         interest_to_count--;
         tte_printf(
@@ -3796,8 +3796,9 @@ static void game_round_end_display_rewards()
     {
         game_round_end_print_hand_reward(hand_y_offset);
     }
-    else if (interest_start_time != UNDEFINED && timer >= interest_start_time &&
-             interest_to_count > 0)
+    else if (
+        interest_start_time != UNDEFINED && timer >= interest_start_time && interest_to_count > 0
+    )
     {
         game_round_end_print_interest_reward(interest_y_offset);
     }
