695237c7b421ce5c64be48ec4893c7ce084c1dcf
Rickey Fehr <ric@rf3.xyz>
2026-03-31 21:21:57 -0700

    tmp commit basic docs

--- include/game/main_menu.h
+++ include/game/main_menu.h
@@ -1,18 +1,31 @@
+/**
+ * @file game_main_menu.h
+ *
+ * @brief Common functions to render UI elements.
+ */
 #ifndef GAME_MAIN_MENU_H
 #define GAME_MAIN_MENU_H
 
 #include "game_variables.h"
 
-// Change the main menu background
+/**
+ * Change the main menu background
+ */
 void game_main_menu_change_background(void);
 
-// Main menu state initialization
+/**
+ * Main menu state initialization
+ */
 void game_main_menu_on_init(GameVariables* vars);
 
-// Main menu state update
+/**
+ * Main menu state update
+ */
 void game_main_menu_on_update(GameVariables* vars);
 
-// Main menu cleanup (called when transitioning to game start)
+/**
+ * Main menu cleanup (called when transitioning to game start)
+ */
 void game_main_menu_on_exit(GameVariables* vars);
 
 #endif // GAME_MAIN_MENU_H
