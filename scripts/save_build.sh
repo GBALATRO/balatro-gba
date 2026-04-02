6454b44b6c34fd261ed87dc97edce86cb5b5b7da
Umut Paklacı <71903884+UmtiPako@users.noreply.github.com>
2026-04-02 04:27:55 +0300

    Partial save functionality, comments & game name env variable

--- scripts/save_build.sh
+++ scripts/save_build.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env bash
 
-make || exit 1
+# NOTE: This script is meant to be run from
+# the root repository directory to work as intended.
+
+set -e
+
+make
+
+GAME_NAME="${GAME_NAME-balatro-gba}"
 timestamp=$(date +%Y%m%d_%H%M%S)
 arg=${1:-"build"}
 dir="saved_builds/${arg}_${timestamp}"
+
 mkdir -p "$dir"
-cp build/balatro-gba.elf build/balatro-gba.gba build/balatro-gba.map "$dir/"
-echo "Build saved to $dir"
+
+failed=0
+  for fe in elf gba map; do
+      if [ -f build/"$GAME_NAME".$fe ]; then
+          cp build/"$GAME_NAME".$fe "$dir/"
+      else
+          echo "Warning: build/$GAME_NAME.$fe not found"
+          failed=1
+      fi
+  done
+
+  if [ $failed -eq 1 ]; then
+      echo "Some files are missing, partially saved to $dir"
+  else
+      echo "Build files saved to $dir"
+  fi
+
+
+
