d9a19e94877b39f840f2192aceee3314c8e16bd4
Umut Paklacı <71903884+UmtiPako@users.noreply.github.com>
2026-04-02 03:11:55 +0300

    added .sh script for copying output files under folder

---
+++ scripts/save_build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+make || exit 1
+timestamp=$(date +%Y%m%d_%H%M%S)
+arg=${1:-"build"}
+dir="saved_builds/${arg}_${timestamp}"
+mkdir -p "$dir"
+cp build/balatro-gba.elf build/balatro-gba.gba build/balatro-gba.map "$dir/"
+echo "Build saved to $dir"
