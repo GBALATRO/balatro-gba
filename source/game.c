MathisMartin31/gba-latro_MathisMartin

fix Deck back side sprite palette

source/game.c
@@ -302,7 +302,7 @@ static ContainedHandTypes _contained_hands = {0};
 GameVariables g_game_vars = {
     .timer = 0, .rng_seed = 0, .rng_step = 0,
 
-    .round = 0, .ante = 0, .money = 0, .deck = DECK_RED,
+    .round = 0, .ante = 0, .money = 0, .deck = DECK_TYPE_RED,
 
     .current_blind = BLIND_TYPE_SMALL,
     .next_boss_blind = BLIND_TYPE_BIG,

GBALATRO/balatro-gba

source/game.c
@@ -267,7 +267,8 @@ static enum PlayState play_state = PLAY_STARTING;
 GameVariables g_game_vars = {
     .timer = 0, .rng_info = {0, 0},
 
-    .round = 0, .ante = 0, .money = 0, .hand_size = DEFAULT_HAND_SIZE, .deck = DECK_RED,
+    .round = 0, .ante = 0, .money = 0, .hand_size = DEFAULT_HAND_SIZE,
+    .deck = DECK_TYPE_RED,
 
     .current_blind = BLIND_TYPE_SMALL,
     .next_boss_blind = BLIND_TYPE_BIG,